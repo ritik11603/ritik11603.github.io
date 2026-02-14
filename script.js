7f3128f0a6607eb4edb337b0eb738ef4ca0e8d13
ritik11603 <ritik11603@gmail.com>
2026-02-15 04:06:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openContact(){
+    document.getElementById("contactBox").style.display="flex";
+}
+function closeContact(){
+    document.getElementById("contactBox").style.display="none";
+}
